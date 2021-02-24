c44effb5f7595115829acdd2d62283386b19491e
Smith Deven M <SmithDevenM@JohnDeere.com>
2021-02-24 15:03:28 -0600

    Added additional Write.csv line to export the summary data created from the summary.R script.

--- export_data.R
+++ export_data.R
@@ -1,1 +1,2 @@
-write.csv(df, file = "Clean_Data.csv") #write to CSV in working directory
+write.csv(df, file = "Clean_Data.csv") #write to CSV the clean data
+write.csv(summary_data, "Summary_Data.csv") #write to CSV the summary data
