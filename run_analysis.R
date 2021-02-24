0d15299ab6c8880da44c1a0f165e38bb2bc25caf
Smith Deven M <SmithDevenM@JohnDeere.com>
2021-02-24 15:03:02 -0600

    Added in source code call for the summary.R file that summarizes data by mean grouped by subject and activity description.

--- run_analysis.R
+++ run_analysis.R
@@ -5,4 +5,5 @@ source("merge_trn_tst.R")  #run script that unions trn and tst data frames
 source("mean_std_filtered.R") #filter for mean and std measurements
 source("activity_names.R") #assign activity names to the data frame
 source("feature_rename.R") #create more intuitive column names
+source("summary.R") #create independent tidy data set with summary data
 source("export_data.R") #export cleaned data to working directory
